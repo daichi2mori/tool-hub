dc853452365f9d1c92de9e8ef78afca72609f58a
daichi2mori <daichi2mori@gmail.com>
2024-05-20 14:49:09 +0900

    fix: Image src & basePath 今度こそ!

--- components/item-card.tsx
+++ components/item-card.tsx
@@ -1,16 +1,12 @@
 import { Item } from "@/types/items";
-import getConfig from "next/config";
 import Image from "next/image";
 import Link from "next/link";
 
 const ItemCard = ({ id, title, tags, href }: Item) => {
-  const { publicRuntimeConfig } = getConfig();
-  const basePath = (publicRuntimeConfig && publicRuntimeConfig.basePath) || "";
-
   return (
     <div className="aspect-video relative p-4 border rounded-md shadow-sm bg-card hover:shadow-lg transition duration-300">
       <div className="aspect-video overflow-hidden relative border mb-2 rounded">
-        <Image fill objectFit="cover" src={`/${id}.avif`} alt="" />
+        <Image fill objectFit="cover" src={`/tool-hub/${id}.avif`} alt="" />
       </div>
       <h2 className="text-sm font-semibold">
         <Link href={href} target="_blank">
@@ -22,7 +18,7 @@ const ItemCard = ({ id, title, tags, href }: Item) => {
         {tags.map((tag) => (
           <Link
             key={tag.id}
-            href={`/tool-hub/${tag.id}`}
+            href={`/${tag.id}`}
             className="border whitespace-nowrap text-muted-foreground bg-muted rounded text-xs px-1.5 py-1"
           >
             {tag.label}
