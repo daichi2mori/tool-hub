d42d9a07960c36aeaaf8b0dba97e33cd7da300cd
daichi2mori <daichi2mori@gmail.com>
2024-05-20 14:06:44 +0900

    試し

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV == "production";
+// const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig = {
-  publicRuntimeConfig: {
-    basePath: isProd ? "/tool-hub" : "",
-  },
+  // publicRuntimeConfig: {
+  //   basePath: isProd ? "/tool-hub" : "",
+  // },
   output: "export",
+  basePath: "/tool-hub",
   images: {
     unoptimized: true,
   },
