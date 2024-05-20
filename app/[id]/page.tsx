e04e331b9fd312254646a67978fce5f98600ceeb
daichi2mori <daichi2mori@gmail.com>
2024-05-20 21:01:27 +0900

    fix: design

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -22,7 +22,7 @@ const Page = ({
 
   return (
     <div className="flex-1 bg-muted/30">
-      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-stretch gap-4">
         {currentItems.map((item) => (
           <ItemCard key={item.id} {...item} />
         ))}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center w-full relative">
+    <div className="relative w-full grid place-items-center">
       <Image src="/tool-hub/status-code/404.png" alt="not found" fill objectFit="contain" />
     </div>
   );
