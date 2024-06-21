475881757d3caa3ef799ec5a9cd7ef9c41d237e8
daichi2mori <daichi2mori@gmail.com>
2024-06-21 21:01:36 +0900

    update: add my web app

--- app/components/side-bar.tsx
+++ app/components/side-bar.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { allCategory } from "@/data/categories";
 import { cn, hasCategory } from "@/lib/utils";
+import { SquareArrowOutUpRightIcon } from "lucide-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -12,7 +13,7 @@ const SideBar = () => {
 
   return (
     <div className="hidden lg:block lg:sticky top-16 z-30 h-[calc(100vh-16rem)] border-r p-4">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 pb-5 border-b">
         {allCategory.map((category) => (
           <Button
             className={cn("justify-start", hasCategory(idParam, category.id) && "bg-accent")}
@@ -23,6 +24,14 @@ const SideBar = () => {
             <Link href={`/${category.id}`}>{category.label}</Link>
           </Button>
         ))}
+      </div>
+      <div className="pt-5">
+        <Button variant="ghost" className="w-full justify-between" asChild>
+          <Link href="https://daichi2mori.github.io/code-shot/" target="_blank" rel="noreferrer">
+            Code Shot
+            <SquareArrowOutUpRightIcon size={14} />
+          </Link>
+        </Button>
       </div>
     </div>
   );
